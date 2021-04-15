2c12fcad92af26f7a27d47596c63a0dfffe08a32
phucpvcntt <phamvanphuccntt@gmail.com>
2021-04-15 16:09:46 +0700

    14 nang cao tinh nang login

---
+++ Model/Dao/UserDao.cs
@@ -0,0 +1,55 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    public class UserDao
+    {
+        OnlineShopDbContext db = null;
+        public UserDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+        public long Insert(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+            return user.ID;
+        }
+
+        public int Login(string userName, string passWord) {
+
+            var result = db.Users.SingleOrDefault(x=>x.UserName == userName);
+
+            if (result == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if (result.Status == false)
+                {
+                    return -1;
+                }
+                else {
+                    if (result.Password == passWord)
+                    {
+                        return 1;
+                    }
+                    else {
+                        return -2;
+                    }
+                }
+                
+            }
+        }
+
+        public User getUserByUserName(string userName) {
+            return db.Users.SingleOrDefault(x=>x.UserName == userName);
+        }
+    }
+}
