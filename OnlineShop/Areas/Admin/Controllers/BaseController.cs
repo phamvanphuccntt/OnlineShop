fb6a0d6155ea9e0810ee43c11af3b9f6ee2d39bb
phucpvcntt <phamvanphuccntt@gmail.com>
2021-04-15 16:32:11 +0700

    15 kiem tra session trong admin

--- OnlineShop/Areas/Admin/Controllers/BaseController.cs
+++ OnlineShop/Areas/Admin/Controllers/BaseController.cs
@@ -1,7 +1,9 @@
 ﻿using OnlineShop.Common;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -10,12 +12,13 @@ namespace OnlineShop.Areas.Admin.Controllers
 {
     public class BaseController : Controller
     {
-        // GET: Admin/Base
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            var session = (UserLogin)Session[Common.CommonConstants.USER_SESSION];
-            if (session == null) {
-                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary ( new { controller = "Login", action = "Index", Area = "Admin" }));
+            var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+            if (session == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new
+                    RouteValueDictionary(new { controller = "Login", action = "Index", Area = "Admin" }));
             }
             base.OnActionExecuting(filterContext);
         }
