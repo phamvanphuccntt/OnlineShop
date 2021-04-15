d3dd8140c389a7cc0c9b59b9f1d9edf4eba13a94
phucpvcntt <phamvanphuccntt@gmail.com>
2021-04-15 17:39:36 +0700

    17 them moi ban ghi vao db

---
+++ OnlineShop/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Model.Dao;
+using Model.EF;
+using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                var encryptor = Encryptor.MD5Hash(user.Password);
+                user.Password = encryptor;
+
+                long id = dao.Insert(user);
+                if (id > 0)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm User thành công");
+                }
+            }
+            return View("Index");
+        }
+    }
+}
