104f33f56e9ecb2080b45ee55ef56c424d0ed0ca
phucpvcntt <phamvanphuccntt@gmail.com>
2021-04-23 10:56:22 +0700

    31 doc du lieu cho cac thanh phan dung chung

--- OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model.Dao;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,25 @@ namespace OnlineShop.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [ChildActionOnly]
+        public ActionResult MainMenu()
+        {
+            var model = new MenuDao().ListByGroupId(1);
+            return PartialView(model);
+        }
+        [ChildActionOnly]
+        public ActionResult TopMenu()
+        {
+            var model = new MenuDao().ListByGroupId(2);
+            return PartialView(model);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Footer()
+        {
+            var model = new FooterDao().getFooter();
+            return PartialView(model);
         }
     }
 }
